384eb48597d5fcbed6292de73865f8e031595d47
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-10-01 22:44:13 -0400

    start

---
+++ ImagePIcker/ImagePIcker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePIcker
+//
+//  Created by albert coelho oliveira on 10/1/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
